6b58f1f7d3650a32da32cade829751e6638d3b6a
David Hutchison <david.hutchison@choreograph.com>
2024-02-16 08:52:16 -0600

    april tag detection class

--- src/detect2.py
+++ src/detect2.py
@@ -2,6 +2,7 @@ import cv2
 import apriltag
 import numpy
 import numpy.typing
+import config
 from dataclasses import dataclass
 
 @dataclass
@@ -10,14 +11,14 @@ class DetectedTag2:
     corners: numpy.typing.NDArray[numpy.float64]
 
 class TagDetector2:
-    def __init__(self, aruco_dict: int):
-        dict = cv2.aruco.getPredefinedDictionary(aruco_dict)
-        params = cv2.aruco.DetectorParameters()
-        self.detector = cv2.aruco.ArucoDetector(dict, params)
+    def __init__(self):
+        params = apriltag.DetectorOptions(config.TagTrackerConfig.tag_family)
+        self.detector = apriltag.Detector()
 
     def detect(self, image) -> list[DetectedTag2]:
-        corners, ids, _ = self.detector.detectMarkers(image)
+        tags = self.detector.detect(image)
         # corners, ids, _ = cv2.aruco.detectMarkers(image, self.dict, parameters=self.params)
-        if len(corners) == 0:
+        if len(tags) == 0:
             return []
-        return [DetectedTag2(id[0], corner) for id, corner in zip(ids, corners)]
+            
+        return [DetectedTag2(tag.tag_id, tag.corner) for tag in tags]
