c8ba32656ef377dc4d79eb174a62375e19809e7b
David Hutchison <david.hutchison@choreograph.com>
2024-02-15 18:00:13 -0600

    update class name for apriltag stub to not brake things

--- src/detect2.py
+++ src/detect2.py
@@ -5,19 +5,19 @@ import numpy.typing
 from dataclasses import dataclass
 
 @dataclass
-class DetectedTag:
+class DetectedTag2:
     id: int
     corners: numpy.typing.NDArray[numpy.float64]
 
-class TagDetector:
+class TagDetector2:
     def __init__(self, aruco_dict: int):
         dict = cv2.aruco.getPredefinedDictionary(aruco_dict)
         params = cv2.aruco.DetectorParameters()
         self.detector = cv2.aruco.ArucoDetector(dict, params)
 
-    def detect(self, image) -> list[DetectedTag]:
+    def detect(self, image) -> list[DetectedTag2]:
         corners, ids, _ = self.detector.detectMarkers(image)
         # corners, ids, _ = cv2.aruco.detectMarkers(image, self.dict, parameters=self.params)
         if len(corners) == 0:
             return []
-        return [DetectedTag(id[0], corner) for id, corner in zip(ids, corners)]
+        return [DetectedTag2(id[0], corner) for id, corner in zip(ids, corners)]
